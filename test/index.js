ffa42809fe89666578157d259dfa3a0d5a6b8251
hrishikeshs <hrish2006@gmail.com>
2015-01-30 19:39:18 +0530

    Update tests.

--- test/index.js
+++ test/index.js
@@ -1,19 +1,15 @@
-// var should = require('chai').should(),
-//     htmlize = require('../index'),
-//     jsdom = require('jsdom').jsdom,
-//     jsonToHtml= htmlize.toHtml;
+var should = require('chai').should(),
+    htmlize = require('../index');
 
-// var dom = jsdom.defaultLevel;
+// QuerySelector must be turned on on the specific document we're creating
 
-
-// // QuerySelector must be turned on on the specific document we're creating
-
-// describe('#generates the dom for the given json', function() {
-//   it('returns the dom json object', function() {
-//     var dom = jsdom.
-//           jsonToHtml({"name": "mongo", "type": "db", "newkey": { "foo" : "bar" } }).should.equal(??)
-
-//   });
-// });
+describe('#generates the dom as a string for the given json', function() {
+  it('returns the dom json object', function() {
+    var dom = htmlize
+          .toHtmlString({"name": "mongo", "type": "db", "newkey": { "foo" : "bar" } })
+          .should
+          .equal('<span>{</span><div style="margin-left: 30px;"><strong>name:  </strong><span>mongo</span>,</div><div style="margin-left: 30px;"><strong>type:  </strong><span>db</span>,</div><div style="margin-left: 30px;"><strong>newkey:  </strong><div><span>{</span><div style="margin-left: 30px;"><strong>foo:  </strong><span>bar</span></div><span>}</span></div></div><span>}</span>');
+  });
+});
 
 
