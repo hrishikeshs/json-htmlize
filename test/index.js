7482db9ac9d71a5cb6647b3910f73196c7a8b269
Hrishikesh S <hrish2006@gmail.com>
2025-03-12 21:48:28 -0700

    Handle `undefined` and `null` values in input
    - Rename param `json` to `jsObject` in the `toHtmlDom` function
    - Rename param `json` to `objToPrettyPrint` in the inner function
    - Rename param `json` to `obj` in the `toHtmlString` function
    - Handle `null` and `undefined` when they appear as values in obj.prop
    - Add test cases and ensure existing ones pass
    - Update dev-dependencies to latest versions
    - Remove `nyan` reporter and rely on plain text output
    - Remove `chai` assertion library in favor of node's built-in assertions
    - Run prettier :)

--- test/index.js
+++ test/index.js
@@ -1,26 +1,52 @@
-var should = require('chai').should(),
-    htmlize = require('../index');
+var htmlize = require('../index');
+var assert = require('node:assert').strict;
 
-describe('json-htmlize', function() {
+describe('json-htmlize', function () {
+  describe('.toHtmlDom()', function () {
+    it('should convert provided JSON to a DOM representation', function () {
+      var domElement = htmlize.toHtmlDom({
+        name: 'mongo',
+        type: 'db',
+        newkey: { foo: 'bar' },
+      });
+      var htmlDomElementAsString = domElement.innerHTML;
+      // asserting that the dom element returned has the innerHTML we expect is enough
+      assert.deepEqual(
+        htmlDomElementAsString,
+        '<span>{</span><div style="margin-left: 30px;"><strong>name:  </strong><span>mongo</span>,</div><div style="margin-left: 30px;"><strong>type:  </strong><span>db</span>,</div><div style="margin-left: 30px;"><strong>newkey:  </strong><div><span>{</span><div style="margin-left: 30px;"><strong>foo:  </strong><span>bar</span></div><span>}</span></div></div><span>}</span>'
+      );
+    });
+  });
 
-	describe('.toHtmlDom()', function() {
+  describe('.toHtmlString()', function () {
+    it('should convert provided JSON to HTML as string', function () {
+      var htmlAsString = htmlize.toHtmlString({
+        name: 'mongo',
+        type: 'db',
+        newkey: { foo: 'bar' },
+      });
 
-		it.skip('should convert provided JSON to a DOM representation', function() {
-			var domElement = htmlize.toHtmlDom({"name": "mongo", "type": "db", "newkey": { "foo" : "bar" } });
+      assert.strictEqual(
+        htmlAsString,
+        '<span>{</span><div style="margin-left: 30px;"><strong>name:  </strong><span>mongo</span>,</div><div style="margin-left: 30px;"><strong>type:  </strong><span>db</span>,</div><div style="margin-left: 30px;"><strong>newkey:  </strong><div><span>{</span><div style="margin-left: 30px;"><strong>foo:  </strong><span>bar</span></div><span>}</span></div></div><span>}</span>'
+      );
+    });
+  });
 
-			// TODO
-		});
+  describe('.toHtmlString handles undefined and null values correctly', function () {
+    it('should convert provided JSON to HTML as string', function () {
+      var htmlAsString = htmlize.toHtmlString({
+        name: 'mongo',
+        type: 'db',
+        newkey: { foo: 'bar' },
+        keyWithNull: null,
+        keyWithUndefined: undefined,
+      });
 
-	});
-
-	describe('.toHtmlString()', function() {
-
-		it('should convert provided JSON to HTML as string', function() {
-			var htmlAsString = htmlize.toHtmlString({"name": "mongo", "type": "db", "newkey": { "foo" : "bar" } });
-
-			htmlAsString.should.equal('<span>{</span><div style="margin-left: 30px;"><strong>name:  </strong><span>mongo</span>,</div><div style="margin-left: 30px;"><strong>type:  </strong><span>db</span>,</div><div style="margin-left: 30px;"><strong>newkey:  </strong><div><span>{</span><div style="margin-left: 30px;"><strong>foo:  </strong><span>bar</span></div><span>}</span></div></div><span>}</span>');
-		});
-
-	});
-
+      assert.strictEqual(
+        htmlAsString,
+        '<span>{</span><div style="margin-left: 30px;"><strong>name:  </strong><span>mongo</span>,</div><div style="margin-left: 30px;"><strong>type:  </strong><span>db</span>,</div><div style="margin-left: 30px;"><strong>newkey:  </strong><div><span>{</span><div style="margin-left: 30px;"><strong>foo:  </strong><span>bar</span></div><span>}</span></div>,</div><div style="margin-left: 30px;"><strong>keyWithNull:  </strong><span>null</span>,</div><div style="margin-left: 30px;"><strong>keyWithUndefined:  </strong><span>undefined</span></div><span>}</span>'
+      );
+    });
+  });
 });
